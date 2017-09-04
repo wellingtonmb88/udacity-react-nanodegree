66b226c5a24e9f948d41e9eb804887ff894253a2
Wellington <nutrinowinc@gmail.com>
2017-09-03 21:53:27 -0300

    Create Meals-Planner project

---
+++ meal-planner/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { connect } from 'react-redux';
+import { addRecipe, removeFromCalendar } from './actions';
+
+class App extends Component {
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
+
+function mapStateToProps (calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+
+        return meals
+      }, {})
+    })),
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
